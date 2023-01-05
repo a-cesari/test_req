859c5c0b6f888c51b5aba54c81bdcffea80ecb8c
Andrea Cesari <andreacesari90@gmail.com>
2023-01-05 15:56:51 +0100

    Update dash.py

--- dash.py
+++ dash.py
@@ -1,6 +1,6 @@
 import requests
 import streamlit as st
-for url in ['https://w3schools.com','https://www.google.it']:
+for url in ['https://w3schools.com','https://www.google.it','https://it.bidoo.com/auction.php?a=Protein_Muesli_Cioccolato_30453593']:
   x = requests.get(url,verify=False,headers={'User-Agent' : "Magic Browser"})
   st.write(f"{url}: {x.status_code}")
 
